19075abc184b29e1adf79f41d51954fff960808a
hahan <hvha1@cmcglobal.vn>
2025-03-12 11:50:23 +0700

    (~) use mysql

--- nest-project/src/app.module.ts
+++ nest-project/src/app.module.ts
@@ -1,15 +1,16 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HttpModule } from '@nestjs/axios';
-import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
-import { ReponseInterceptor } from './interceptors/reponse.interceptor';
 import { HttpExceptionFilter } from './exceptions/http-exception';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { FilesController } from './files/files.controller';
 import { FilesModule } from './files/files.module';
 import { FilesService } from './files/files.service';
+import { ReponseInterceptor } from './interceptors/reponse.interceptor';
 
 @Module({
   imports: [
@@ -18,6 +19,15 @@ import { FilesService } from './files/files.service';
       rootPath: join(__dirname, '..'),
     }),
     FilesModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'perfumes-store',
+      synchronize: true,
+    }),
   ],
   controllers: [AppController, FilesController],
   providers: [
