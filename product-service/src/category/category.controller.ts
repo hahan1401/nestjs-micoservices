d8120ef6355062174d095cde84e5afd9b055f0c9
hahan <hvha1@cmcglobal.vn>
2025-03-20 10:45:48 +0700

    (~) back to monolythic

--- product-service/src/category/category.controller.ts
+++ product-service/src/category/category.controller.ts
@@ -1,40 +1,15 @@
-import { Controller, Inject } from '@nestjs/common';
-import {
-  ClientProxy,
-  ClientProxyFactory,
-  MessagePattern,
-  Transport,
-} from '@nestjs/microservices';
-import { lastValueFrom } from 'rxjs';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { ResponseDTO } from 'src/DTO/response';
 import { CategoryService } from './category.service';
 import { Category } from './chemas/category.schema';
 
 @Controller('category')
 export class CategoryController {
-  private client: ClientProxy;
-
   @Inject()
   private readonly categoryService: CategoryService;
 
-  constructor() {
-    this.client = ClientProxyFactory.create({
-      transport: Transport.TCP,
-      options: {
-        port: 4002,
-        host: 'payment-service',
-      },
-    });
-  }
-
-  @MessagePattern('productService.category.getAll')
+  @Get()
   async getAll(): Promise<ResponseDTO<Category[]>> {
-    const a = await this.client.send('asd', {}).forEach((value) => {
-      console.log('value', value);
-    });
-    console.log('a', a);
-    const s = await lastValueFrom(this.client.send('asd', {}));
-    console.log('s', s);
     return this.categoryService.getAll();
   }
 }
